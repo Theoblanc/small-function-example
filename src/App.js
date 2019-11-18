97eb89cc35e17055fb781326049a758741c85fc9
JungTaeHyun <xogus1015@luchwit.com>
2019-11-18 18:07:09 +0900

    scroll, onFocus, onBulr event

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  background-color: black;
+  height: 200vh;
+  padding: 0;
+  margin: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30vw;
+  height: 30vh;
+`;
+
+const InputWrap = styled.div``;
+
+const Input = styled.input`
+  background-color: ${props =>
+    props.focuseColor ? "palevioletred" : "#ffffff"};
+`;
+
+const App = () => {
+  const [focused, setFocused] = useState(false);
+
+  const onFocuses = e => {
+    const focuse = e.target;
+    if (focuse) {
+      setFocused(true);
+    }
+  };
+
+  const onBlures = e => {
+    setFocused(false);
+  };
+
+  const mouseEvent = e => {
+    const scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop;
+
+    console.log(scrollTop);
+  };
+
+  console.log(focused);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container onWheel={mouseEvent}>
+      <Wrap>
+        <InputWrap>
+          <Input
+            onFocus={onFocuses}
+            onBlur={onBlures}
+            focuseColor={focused}
+          ></Input>
+        </InputWrap>
+      </Wrap>
+    </Container>
   );
-}
+};
 
 export default App;
